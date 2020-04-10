83366c1ce29534a9e6de427414b1f1e2e1ae47b2
Neeraj1993 <kush.neeraj1993@gmail.com>
2020-04-10 17:19:45 +0530

    Updates

---
+++ precache-manifest.889076d5534d4f88948954c294b21c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b74a3a90921578e970819dea7f8e091",
+    "url": "/contactlist/index.html"
+  },
+  {
+    "revision": "0081f694501066847b38",
+    "url": "/contactlist/static/css/main.60709668.chunk.css"
+  },
+  {
+    "revision": "cb6825c067ada174141f",
+    "url": "/contactlist/static/js/2.9f2e3466.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/contactlist/static/js/2.9f2e3466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0081f694501066847b38",
+    "url": "/contactlist/static/js/main.ba7f3a30.chunk.js"
+  },
+  {
+    "revision": "c1acb6d559bb81543ab8",
+    "url": "/contactlist/static/js/runtime-main.62827205.js"
+  }
+]);
